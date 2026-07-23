75eecf8b8e93e6c3299d3613b7696a649216318b
JasonMadness <tobc_madness@mail.ru>
2026-07-23 17:24:11 +0300

    add player movement script
    and scripts folder

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 7f;
+    [SerializeField] private float _jumpForce = 15f;
+
+    private Rigidbody2D _rigidbody2D;
+    private float _horizontalInput;
+    private bool _isGrounded;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
+            _isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(_horizontalInput * _moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = movement;
+        CheckGround();
+    }
+
+    private void CheckGround()
+    {
+        float groundCheckRadius = 0.3f;
+        LayerMask groundLayer = LayerMask.GetMask("Default", "Ground");
+        _isGrounded = Physics2D.OverlapCircle(transform.position, groundCheckRadius, groundLayer);
+    }
+}
