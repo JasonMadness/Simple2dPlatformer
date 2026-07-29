8ef46b01b2590f0d44c0f6ab97e8cc6696bed8d6
JasonMadness <tobc_madness@mail.ru>
2026-07-29 17:25:42 +0300

    Updated enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,12 +1,46 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float _xBoundary = 4f;
+    [SerializeField] private float _speed = 2f;
 
-    private void Move()
+    private float _direction = 1f;
+    private Vector2 _startPosition;
+
+    private float _minX;
+    private float _maxX;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
     {
-        float xPosition = Mathf.PingPong(Time.time, _xBoundary) - _xBoundary;
-        transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _minX = _startPosition.x - _xBoundary;
+        _maxX = _startPosition.x + _xBoundary;
+    }
+
+    private void Update()
+    {
+        float moveX = _direction * _speed * Time.deltaTime;
+        transform.Translate(moveX, 0f, 0f);
+
+        float currentX = transform.position.x;
+
+        if (currentX <= _minX || currentX >= _maxX)
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        _direction *= -1f;
+        _spriteRenderer.flipX = _direction > 0;
     }
 }
