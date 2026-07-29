84d81935069d81195d42794e8fbf675af2a638ac
JasonMadness <tobc_madness@mail.ru>
2026-07-29 18:55:56 +0300

    added player detection system

---
+++ Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out _))
+            Debug.Log("Player detected!");
+    }
+}
