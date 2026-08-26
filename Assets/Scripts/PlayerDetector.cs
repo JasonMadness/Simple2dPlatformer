f7af6e24273c4f51f75d2cf563053feb8d5c217f
JasonMadness <tobc_madness@mail.ru>
2026-08-27 01:20:25 +0300

    Update PlayerDetector.cs

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerDetector : MonoBehaviour
 {
     [SerializeField] private float _detectionRange = 3f;
     [SerializeField] private float _sameLevelTolerance = 0.5f;
 
     private Transform _player;
+    private bool _isPlayerDetected;
 
     public Transform Player => _player;
 
-    private void Awake()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-        if (playerObject != null)
-            _player = playerObject.transform;
+        if (other.gameObject.TryGetComponent<Player>(out _))
+        {
+            _player = other.transform;
+            _isPlayerDetected = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent<Player>(out _))
+        {
+            _isPlayerDetected = false;
+            _player = null;
+        }
     }
 
     public bool IsPlayerDetected()
     {
-        if (_player == null)
-            return false;
-
-        float horizontalDistance = Mathf.Abs(_player.position.x - transform.position.x);
-        float verticalDistance = Mathf.Abs(_player.position.y - transform.position.y);
-
-        return horizontalDistance <= _detectionRange && verticalDistance <= _sameLevelTolerance;
+        return _isPlayerDetected && _player != null;
     }
 }
+
