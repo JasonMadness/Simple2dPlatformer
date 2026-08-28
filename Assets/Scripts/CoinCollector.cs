ee418eb93c76fc2bf05810a015cf6a645359187a
JasonMadness <tobc_madness@mail.ru>
2026-08-28 20:35:22 +0300

    Update CoinCollector.cs

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -1,10 +1,7 @@
-using System;
 using UnityEngine;
 
 public class CoinCollector : MonoBehaviour
 {
-    public event Action Collected;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Coin coin))
