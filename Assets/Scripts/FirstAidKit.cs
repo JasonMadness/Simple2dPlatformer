cbc2479b4715e813694162d2e9ab758305f38b39
JasonMadness <tobc_madness@mail.ru>
2026-08-28 20:47:15 +0300

    Update FirstAidKit.cs

--- Assets/Scripts/FirstAidKit.cs
+++ Assets/Scripts/FirstAidKit.cs
@@ -4,12 +4,5 @@ public class FirstAidKit : MonoBehaviour
 {
     [SerializeField] private int _healAmount = 20;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent(out Health health))
-        {
-            health.Heal(_healAmount);
-            Destroy(gameObject);
-        }
-    }
+    public int HealAmount => _healAmount;
 }
