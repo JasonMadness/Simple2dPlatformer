9234bc93c60d93c2728186e88f653ffa5da1a839
JasonMadness <tobc_madness@mail.ru>
2026-08-28 21:01:17 +0300

    Update CharacterRotation.cs

--- Assets/Scripts/CharacterRotation.cs
+++ Assets/Scripts/CharacterRotation.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Face(float direction)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float yRotation = direction > 0f ? 0f : 180f;
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
     }
 }
