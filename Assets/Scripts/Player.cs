7c8439ee4f73260f01fc5b75840d9bcabf1fbc81
JasonMadness <tobc_madness@mail.ru>
2026-08-28 20:46:34 +0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Player : MonoBehaviour
 {
+    private Health _health;
 
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        _health.Died += OnDied;
+    }
+
+    private void OnDisable()
+    {
+        _health.Died -= OnDied;
+    }
+
+    private void OnDied()
+    {
+        gameObject.SetActive(false);
+    }
 }
