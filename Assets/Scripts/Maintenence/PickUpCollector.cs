8bc6688c03069d7bbfdb3e476462c8514daac2e8
JasonMadness <tobc_madness@mail.ru>
2026-08-29 10:33:10 +0300

    Update PickUpCollector.cs

--- Assets/Scripts/Maintenence/PickUpCollector.cs
+++ Assets/Scripts/Maintenence/PickUpCollector.cs
@@ -1,11 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class PickUpCollector : MonoBehaviour
 {
+    private Health _health;
+
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.TryGetComponent(out PickUp pickUp)
+        {
+            if (pickUp is Coin)
+            {
+                pickUp.Collect();
+            }
 
+            else if (pickUp is FirstAidKit)
+            {
+                _health.Heal(pickUp.HealAmount);
+                Destroy(pickUp.gameObject);
+            }
     }
 }
