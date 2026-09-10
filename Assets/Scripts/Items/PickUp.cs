f3bbfa93fc98d5d845cbb0d718ed0262420b5f35
JasonMadness <tobc_madness@mail.ru>
2026-09-10 18:55:59 +0300

    Update PickUp.cs

--- Assets/Scripts/Items/PickUp.cs
+++ Assets/Scripts/Items/PickUp.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    [SerializeField] private int _healAmount;
-
-    public int HealAmount => _healAmount;
-
     public event Action<PickUp> Collected;
 
     public void Collect()
