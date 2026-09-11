76ce9f844a719c2eb0085a0baff51cc702657431
JasonMadness <tobc_madness@mail.ru>
2026-09-11 07:33:46 +0300

    finding bugs

--- Assets/Scripts/UI/SmoothBar.cs
+++ Assets/Scripts/UI/SmoothBar.cs
@@ -13,6 +13,8 @@ public class SmoothBar : Bar
     {
         if (Mathf.Abs(_slider.value - _targetValue) > _epsilon)
             _slider.value = Mathf.MoveTowards(_slider.value, _targetValue, _speed * Time.deltaTime);
+
+        Debug.Log(_slider.value + " " + _targetValue);
     }
 
     protected override void UpdateView()
