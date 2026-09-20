ef2b4ca8d7aa7c5f32f596c7cb4bc8dbf8c2c2b9
JasonMadness <tobc_madness@mail.ru>
2026-09-20 10:21:57 +0300

    Update EventSubscriber.cs

--- Assets/Scripts/Maintenence/EventSubscriber.cs
+++ Assets/Scripts/Maintenence/EventSubscriber.cs
@@ -4,19 +4,24 @@ public class EventSubscriber : MonoBehaviour
 {
     [SerializeField] private Health _playerHealth;
     [SerializeField] private Bar _playerHealthBar;
+    [SerializeField] private Health _enemyHealth;
+    [SerializeField] private Bar _enemyHealthBar;
 
     private void Start()
     {
         _playerHealthBar.Initialize(_playerHealth.Max, _playerHealth.Current);
+        _enemyHealthBar.Initialize(_enemyHealth.Max, _enemyHealth.Current);
     }
 
     private void OnEnable()
     {
         _playerHealth.ValueChanged += _playerHealthBar.OnValueChanged;
+        _enemyHealth.ValueChanged += _enemyHealthBar.OnValueChanged;
     }
 
     private void OnDisable()
     {
         _playerHealth.ValueChanged -= _playerHealthBar.OnValueChanged;
+        _enemyHealth.ValueChanged -= _enemyHealthBar.OnValueChanged;
     }
 }
