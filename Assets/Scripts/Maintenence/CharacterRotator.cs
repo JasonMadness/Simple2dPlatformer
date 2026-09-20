d2e36b8ff42891cf0a5f17503867ec65f2119b0b
JasonMadness <tobc_madness@mail.ru>
2026-09-20 10:18:34 +0300

    Update CharacterRotator.cs

--- Assets/Scripts/Maintenence/CharacterRotator.cs
+++ Assets/Scripts/Maintenence/CharacterRotator.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class CharacterRotator : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void Face(float direction)
     {
-        float yRotation = direction > 0f ? 0f : 180f;
-        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        _spriteRenderer.flipX = direction < 0f;
     }
 }
