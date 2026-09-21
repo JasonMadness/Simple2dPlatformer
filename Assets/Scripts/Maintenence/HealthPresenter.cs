521886bebba87b06a096965139b16a34a2a52acf
JasonMadness <tobc_madness@mail.ru>
2026-09-21 19:32:59 +0300

    Update HealthPresenter.cs

--- Assets/Scripts/Maintenence/HealthPresenter.cs
+++ Assets/Scripts/Maintenence/HealthPresenter.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HealthPresenter : MonoBehaviour
+public class HealthPresenter
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly Health _health;
+    private readonly Bar _bar;
+
+    public HealthPresenter(Health health, Bar bar)
     {
-        
+        _health = health;
+        _bar = bar;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Enable()
     {
-        
+        _bar.Initialize(_health.Max, _health.Current);
+        _health.ValueChanged += _bar.OnValueChanged;
+    }
+
+    public void Disable()
+    {
+        _health.ValueChanged -= _bar.OnValueChanged;
     }
 }
