1b6c483c5196514d872ea2105a0eb55f0a09437a
JasonMadness <tobc_madness@mail.ru>
2026-09-21 23:20:39 +0300

    Create base Character.cs

---
+++ Assets/Scripts/Maintenence/Character.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+RequireComponent(typeof(Health))]
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
