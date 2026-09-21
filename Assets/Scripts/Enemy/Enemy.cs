79bb7b703e45fca12a3759f62fd65262fa32e478
JasonMadness <tobc_madness@mail.ru>
2026-09-21 23:21:55 +0300

    Update Player.cs & Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,27 +1,4 @@
-using UnityEngine;
-
-[RequireComponent(typeof(Health))]
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
-    private Health _health;
-
-    private void Awake()
-    {
-        _health = GetComponent<Health>();
-    }
-
-    private void OnEnable()
-    {
-        _health.Ended += OnHealthEnded;
-    }
-
-    private void OnDisable()
-    {
-        _health.Ended -= OnHealthEnded;
-    }
-
-    private void OnHealthEnded()
-    {
-        gameObject.SetActive(false);
-    }
+    
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,27 +1,4 @@
-using UnityEngine;
-
-[RequireComponent(typeof(Health))]
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    private Health _health;
-
-    private void Awake()
-    {
-        _health = GetComponent<Health>();
-    }
-
-    private void OnEnable()
-    {
-        _health.Ended += OnHealthEnded;
-    }
-
-    private void OnDisable()
-    {
-        _health.Ended -= OnHealthEnded;
-    }
-
-    private void OnHealthEnded()
-    {
-        gameObject.SetActive(false);
-    }
+    
 }
