a6df855ab5b8f2a99a754ebd94f87f47f26b68e3
JasonMadness <tobc_madness@mail.ru>
2026-09-21 22:57:31 +0300

    Update VampirismDamager.cs

--- Assets/Scripts/Player/VampirismDamager.cs
+++ Assets/Scripts/Player/VampirismDamager.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VampirismDamager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _radius = 4f;
+    [SerializeField] private float _damagePerSecond = 20f;
+    [SerializeField] private LayerMask _enemyLayer;
+
+    private float _accumulatedDamage;
+
+    public float Radius => _radius;
+
+    public int Damage(float deltaTime)
     {
-        
+        Health target = FindNearestTarget();
+
+        if (target == null)
+            return 0;
+
+        _accumulatedDamage += _damagePerSecond * deltaTime;
+
+        int damage = Mathf.FloorToInt(_accumulatedDamage);
+
+        if (damage <= 0)
+            return 0;
+
+        _accumulatedDamage -= damage;
+        target.Decrease(damage);
+
+        return damage;
     }
 
-    // Update is called once per frame
-    void Update()
+    private Health FindNearestTarget()
     {
-        
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _radius, _enemyLayer);
+
+        Health nearestTarget = null;
+        float nearestDistance = float.MaxValue;
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.TryGetComponent(out Health health) == false)
+                continue;
+
+            float distance = Vector2.SqrMagnitude((Vector2)health.transform.position - (Vector2)transform.position);
+
+            if (distance > _radius * _radius)
+                continue;
+
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestTarget = health;
+            }
+        }
+
+        return nearestTarget;
     }
 }
