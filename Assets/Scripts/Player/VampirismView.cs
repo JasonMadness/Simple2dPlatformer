4b1352ce207f535cd4aecc749064864caaccd7e1
JasonMadness <tobc_madness@mail.ru>
2026-09-21 22:57:10 +0300

    Update VampirismView.cs

--- Assets/Scripts/Player/VampirismView.cs
+++ Assets/Scripts/Player/VampirismView.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VampirismView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer _area;
+    [SerializeField] private Slider _chargeSlider;
+
+    public void SetRadius(float radius)
     {
-        
+        Sprite sprite = _area.sprite;
+        float spriteSize = sprite == null ? 1f : sprite.bounds.size.x;
+
+        _area.transform.localScale = Vector3.one * (radius * 2f / spriteSize);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Show()
     {
-        
+        _area.enabled = true;
+    }
+
+    public void Hide()
+    {
+        _area.enabled = false;
+    }
+
+    public void SetCharge(float value)
+    {
+        _chargeSlider.value = value;
     }
 }
